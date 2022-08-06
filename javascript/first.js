c6700503032a9e3b62143bac4965b322bac1cee1
mihir hadavani <miheerhadvani990@gmail.com>
2022-08-06 14:08:08 +0530

    modifiy

---
+++ javascript/first.js
@@ -0,0 +1,18 @@
+var age = 19;
+var name = "mihir";
+
+
+console.log("age = ", age);
+
+console.log("name  = ", name);
+var cla = {
+    nam: "mihir",
+    ag: 18
+}; // object
+console.log(cla);
+
+
+var arr = [1, 2, 3, 4];
+console.log(cla.nam);
+
+console.log(arr[0]);
